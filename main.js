2770ab286c0906df1593f10390dd63f53ca5b01e
Paschal Corrigan <pcorrigan@umass.edu>
2022-08-09 01:41:39 -0400

    Initialize Calendar.

---
+++ main.js
@@ -0,0 +1,17 @@
+const dates = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+function renderCal(element) {
+element.innerHTML = '';
+for (let i = 0; i < 5; ++i) {
+    for (let j = 0; j < 7; ++j) {
+        let date = parseInt("" + i + j - 9);
+        const div = document.createElement('div');
+        if (i === 0) { div.innerText = dates[j]; }
+        else if (date > 31) { continue; }
+        else { div.innerText = date; }
+        div.classList.add('calendar-item');
+        element.appendChild(div);
+    }
+}
+}
+
+renderCal(calendar);
